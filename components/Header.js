7c047dbc4d231a37d3fa3f51c7b579f7a6540ec4
programmermad7697 <ostuff185@gmail.com>
2022-06-17 17:42:24 +0530

    Swimming Madness - Next.js, TailwindCSS

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,6 @@
 import Link from 'next/link'
-import { useMoralis } from 'react-moralis'
 
 function Header() {
-  const { authenticate, isAuthenticated, logout } = useMoralis()
-
   return (
     <header className="sticky flex h-24 w-full justify-between bg-gradient-to-r from-blue-200 via-blue-300 to-blue-500 p-7 text-white">
       {/* Left */}
@@ -16,24 +13,12 @@ function Header() {
       {/* Right */}
       <div className="flex space-x-7">
         <h1 className="text-2xl font-bold">
-          <Link href={'/logBook'}>Log Book</Link>
+          <Link href={'/review'}>Review</Link>
         </h1>
 
-        {!isAuthenticated ? (
-          <button
-            className="rounded-full bg-white px-5 py-3 text-center text-xl font-bold text-black"
-            onClick={() => authenticate()}
-          >
-            Login
-          </button>
-        ) : (
-          <button
-            className="rounded-full bg-white px-5 py-3 text-center text-xl font-bold text-black"
-            onClick={() => logout()}
-          >
-            Logout
-          </button>
-        )}
+        <button className="rounded-full bg-white px-4 py-2 text-xs font-bold text-black lg:px-5 lg:py-3 lg:text-base">
+          Login
+        </button>
       </div>
     </header>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,8 @@
-import { MoralisProvider } from 'react-moralis'
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <MoralisProvider
-      serverUrl="https://ynoum6e4e0je.usemoralis.com:2053/server"
-      appId="1sz7xNFlywLtZkekS7UEStxwcGlpzgttNU0v000A"
-    >
-      <Component {...pageProps} />
-    </MoralisProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
